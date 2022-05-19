c5023b16ba94cad5bd6d59a75d540ffebbdac638
AleGarQ <47890663+AleGarQ@users.noreply.github.com>
2022-05-19 13:24:33 -0500

    routes added

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const schemas = require('../models/schemas')
+const validate = require('../middleware/validate')
+const userController = require('../controllers/users')
+
+router.post('/register', validate(schemas.user), userController.create)
+router.post('/login', userController.login)
+router.get('/', userController.index)
+router.get('/:name', function(req, res){
+    res.send('respond with user id: ' + req.params.name)
+})
+
+
+module.exports = router
---
+++ routes/vehicles.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const schemas = require('../models/schemas')
+const validate = require('../middleware/validate')
+const auth = require('../middleware/auth')
+const vehicleController = require('../controllers/vehicles')
+
+router.post('/', auth, validate(schemas.vehicle), vehicleController.create)
+router.put('/:id', auth, vehicleController.update)
+router.get('/', auth, vehicleController.index)
+router.get('/:id', auth, vehicleController.show)
+router.delete('/:id', auth, vehicleController.delete)
+
+
+module.exports = router
