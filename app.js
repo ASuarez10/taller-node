979acef2eded73ea65b4481b49f87cceffa62bdb
AleGarQ <47890663+AleGarQ@users.noreply.github.com>
2022-05-19 16:28:33 -0500

    fix some errors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require("mongoose")
 
-var indexRouter = require('./routes/index.js/index');
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var vehiclesRouter = require('./routes/vehicles');
 
